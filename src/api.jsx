5021f6975de98eee850bf23aa442e792f0e0d2d2
Ford1508 <phuclc1508@gmail.com>
2023-06-22 17:03:31 +0700

    modify API URL

--- src/api.jsx
+++ src/api.jsx
@@ -1,3 +1,3 @@
 export const api = `${import.meta.env.VITE_API_BASE_URL}/api`
-export const api_ai = `${import.meta.env.VITE_API_BASE_URL}:5000`
+export const api_ai = `${import.meta.env.VITE_AI_BASE_URL}`
 export const api_image = `${import.meta.env.VITE_API_BASE_URL}`
